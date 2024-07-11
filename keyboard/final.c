707ed915e3b142d0ab1e918a8b85ec7a78f44d0b
Aditya Gautam <adityascottish27@gmail.com>
2024-07-11 23:59:09 +0530

    Updated Dots

--- keyboard/final.c
+++ keyboard/final.c
@@ -8,8 +8,10 @@
  *     14 15  16 17
  */
 
-const char layout_alpha_2 = '²';
-const char layout_num = '¹';
+const char layout_alpha_2 = '└';
+const char layout_num = '┴';
+const char layout_symbols = '┬';
+const char layout_symbols_2 = '├';
 
 int pins[] = {2, 3, 4,  5,  10, 11, 12, 13, 6,  7,
               8, 9, 21, 20, 19, 18, 14, 15, 16, 17};
@@ -21,24 +23,49 @@ bool key_down[num_pins];
 // LAYOUT
 char alpha_1[] = {
 
-    layout_num, 'l', 'g', 'd', 'h', 'u', 'o', KEY_RETURN,     'i',
-    's',        'r', 't', 'n', 'e', 'a', 'c', layout_alpha_2, KEY_BACKSPACE,
-    ' ',        ' '
+    layout_num, 'l', 'g', 'd',           'h', 'u', 'o', KEY_RETURN, 
+    'i',        's', 'r', 't',           'n', 'e', 'a', 'c', 
+layout_alpha_2, KEY_BACKSPACE,           ' ', layout_symbols
 
 };
 
 char alpha_2[] = {
 
-    KEY_RETURN, 'v', 'w',     'm',           'f', '\'',   'z',
-    KEY_RETURN, 'q', 'j',     'p',           'k', 'b',    ',',
-    'x',        'y', KEY_TAB, KEY_CAPS_LOCK, ' ', KEY_TAB
+    '.', 'v', 'w', 'm',           'f', '\'', 'z', '"', 
+    'q',        'j', 'p', 'k',           'b',  ',', 'x', 'y', 
+       KEY_TAB, ' ',                      KEY_CAPS_LOCK,  KEY_TAB
 
 };
 
 char num[] = {
 
-    KEY_RETURN, ' ', '+', '-', '1', '2', '3', '4', ' ', ' ', '/',
-    '*',        '5', '6', '7', '8', '<', '>', '9', '0'
+    KEY_RETURN, ' ', '+', '-',           '1', '2', '3', '4', 
+    ' ',        ' ', '/', '*',           '5',  '6', '7', '8', 
+       '=', '.',           '9',  '0'
+
+};
+
+char symbols[] = {
+
+    '/', '?', '#', ':',        '(', ')', '[', ']', 
+    '~', '_', '!', ';',        '{',  '}', '<', '>', 
+  layout_symbols_2, '%',        ' ',  '0'
+
+};
+
+char symbols_2[] = {
+
+    '`', '@', '#', '$',        ';', '\'', ',', '-', 
+    '%', '^', '\\', '|',        ':', '"', '.', '+', 
+              '*', '&',        ' ',  '0'
+
+};
+
+char sys[] = {
+
+    KEY_LEFT_CTRL, '@', '#', '$',        ';', '\'', ',', '-', 
+    KEY_LEFT_SHIFT, '^', '\\', '|',        ':', '"', '.', '+', 
+              '*', '&',        ' ',  '0'
 
 };
 
@@ -63,6 +90,22 @@ void loop() {
             if (digitalRead(pins[i]) == LOW) {
               if (!key_down[i] && alpha_1[i] != layout_alpha_2) {
                 Keyboard.write(alpha_2[i]);
+              }
+              key_down[i] = true;
+            } else {
+              key_down[i] = false;
+            }
+          }
+          delay(10);
+        }
+      } else if (alpha_1[i] == layout_symbols) {  // symbol 1 layer
+        while (digitalRead(pins[i]) == LOW) {
+          for (int i = 0; i < num_pins; i++) {
+            if (digitalRead(pins[i]) == LOW) {
+              if (!key_down[i] && alpha_1[i] != layout_symbols && digitalRead(14) == LOW && symbols[i] != layout_symbols_2) {
+                Keyboard.write(symbols_2[i]);
+              } else if (!key_down[i] && alpha_1[i] != layout_symbols && symbols[i] != layout_symbols_2) {
+                Keyboard.write(symbols[i]);
               }
               key_down[i] = true;
             } else {
@@ -98,3 +141,4 @@ void loop() {
 
   delay(10);
 }
+
