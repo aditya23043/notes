f5ed708550c8e795c929780dae6522fd93078b7e
Aditya Gautam <adityascottish27@gmail.com>
2024-07-12 00:40:05 +0530

    Updated Dots

--- keyboard/final.c
+++ keyboard/final.c
@@ -12,6 +12,7 @@ const char layout_alpha_2 = '└';
 const char layout_num = '┴';
 const char layout_symbols = '┬';
 const char layout_symbols_2 = '├';
+const char layout_sys = '─';
 
 int pins[] = {2, 3, 4,  5,  10, 11, 12, 13, 6,  7,
               8, 9, 21, 20, 19, 18, 14, 15, 16, 17};
@@ -23,7 +24,7 @@ bool key_down[num_pins];
 // LAYOUT
 char alpha_1[] = {
 
-    layout_num, 'l', 'g', 'd',           'h', 'u', 'o', KEY_RETURN, 
+    layout_num, 'l', 'g', 'd',           'h', 'u', 'o', layout_sys, 
     'i',        's', 'r', 't',           'n', 'e', 'a', 'c', 
 layout_alpha_2, KEY_BACKSPACE,           ' ', layout_symbols
 
@@ -63,9 +64,9 @@ char symbols_2[] = {
 
 char sys[] = {
 
-    KEY_LEFT_CTRL, '@', '#', '$',        ';', '\'', ',', '-', 
-    KEY_LEFT_SHIFT, '^', '\\', '|',        ':', '"', '.', '+', 
-              '*', '&',        ' ',  '0'
+    KEY_MENU,       KEY_UP_ARROW,   ' ',             KEY_LEFT_SHIFT,         KEY_RETURN, KEY_TAB, ' ', ' ', 
+    KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_LEFT_CTRL,          KEY_CAPS_LOCK, ' ', ' ', ' ', 
+                                       KEY_LEFT_GUI, KEY_RIGHT_ALT,          KEY_RIGHT_ALT, KEY_RIGHT_GUI
 
 };
 
@@ -74,11 +75,18 @@ void setup() {
     key_down[i] = false;
     pinMode(pins[i], INPUT_PULLUP);
   }
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-int count = 0;
+bool modifier_key_on = false;
 
 void loop() {
+
+  if(modifier_key_on){
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 
   for (int i = 0; i < num_pins; i++) {
 
@@ -90,6 +98,10 @@ void loop() {
             if (digitalRead(pins[i]) == LOW) {
               if (!key_down[i] && alpha_1[i] != layout_alpha_2) {
                 Keyboard.write(alpha_2[i]);
+                if(modifier_key_on){
+                  Keyboard.releaseAll();
+                  modifier_key_on = false;
+                }
               }
               key_down[i] = true;
             } else {
@@ -104,8 +116,16 @@ void loop() {
             if (digitalRead(pins[i]) == LOW) {
               if (!key_down[i] && alpha_1[i] != layout_symbols && digitalRead(14) == LOW && symbols[i] != layout_symbols_2) {
                 Keyboard.write(symbols_2[i]);
+                if(modifier_key_on){
+                  Keyboard.releaseAll();
+                  modifier_key_on = false;
+                }
               } else if (!key_down[i] && alpha_1[i] != layout_symbols && symbols[i] != layout_symbols_2) {
                 Keyboard.write(symbols[i]);
+                if(modifier_key_on){
+                  Keyboard.releaseAll();
+                  modifier_key_on = false;
+                }
               }
               key_down[i] = true;
             } else {
@@ -120,6 +140,33 @@ void loop() {
             if (digitalRead(pins[i]) == LOW) {
               if (!key_down[i] && alpha_1[i] != layout_num) {
                 Keyboard.write(num[i]);
+                if(modifier_key_on){
+                  Keyboard.releaseAll();
+                  modifier_key_on = false;
+                }
+              }
+              key_down[i] = true;
+            } else {
+              key_down[i] = false;
+            }
+          }
+          delay(10);
+        }
+      } else if (alpha_1[i] == layout_sys) { // sys layer
+        while (digitalRead(pins[i]) == LOW) {
+          for (int i = 0; i < num_pins; i++) {
+            if (digitalRead(pins[i]) == LOW) {
+              if (!key_down[i] && alpha_1[i] != layout_sys && sys[i] != ' ') {
+                if(sys[i] == KEY_LEFT_SHIFT || sys[i] == KEY_LEFT_CTRL || sys[i] == KEY_LEFT_GUI || sys[i] == KEY_RIGHT_SHIFT || sys[i] == KEY_RIGHT_CTRL || sys[i] == KEY_RIGHT_GUI){
+                  Keyboard.press(sys[i]);
+                  modifier_key_on = true;
+                } else {
+                  Keyboard.write(sys[i]);
+                  if(modifier_key_on){
+                    Keyboard.releaseAll();
+                    modifier_key_on = false;
+                  }
+                }
               }
               key_down[i] = true;
             } else {
@@ -131,6 +178,10 @@ void loop() {
       } else { // alpha 1
         if (!key_down[i]) {
           Keyboard.write(alpha_1[i]);
+          if(modifier_key_on){
+            Keyboard.releaseAll();
+            modifier_key_on = false;
+          }
         }
         key_down[i] = true;
       }
@@ -141,4 +192,5 @@ void loop() {
 
   delay(10);
 }
+
 
