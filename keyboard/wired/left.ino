882dfcf4f35bb799306fee664a8d8b8799a1602a
Aditya Gautam <adityascottish27@gmail.com>
2024-07-26 13:13:02 +0530

    Updated

--- keyboard/wired/left.ino
+++ keyboard/wired/left.ino
@@ -1,7 +1,29 @@
 // LEFT HALF
+#include <Keyboard.h>
 
 #define rx_pin 1
 #define tx_pin 0
+
+char layout_alpha_2_key = 'ÿ';
+char layout_symbol_key = 'þ';
+char layout_num_key = 'ý';
+char layout_system_key = 'ü';
+
+int pins[] = {
+  18, 19, 20, 21,
+  13, 12, 11, 10,
+          14, 15
+};
+const int num_pins = sizeof(pins)/sizeof(pins[0]);
+
+char key_down[num_pins];
+
+char alpha_1[] = {
+  layout_num_key, 'l', 'g', 'd',
+             'i', 's', 'r', 't',
+             layout_alpha_2_key, KEY_BACKSPACE
+};
+
 
 String foo;
 
@@ -10,8 +32,12 @@ void setup() {
   Serial1.setRX(rx_pin);
   Serial1.setTX(tx_pin);
   Serial1.begin(115200);
+  Keyboard.begin();
 
-  Serial.begin(9600);
+  for(int i = 0; i < num_pins; i++){
+    pinMode(pins[i], INPUT_PULLUP);
+    key_down[i] = false;
+  }
 
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -19,14 +45,18 @@ void setup() {
 
 void loop() {
 
-  if(Serial1.available()){
-    foo = Serial1.readStringUntil('\n');
-    if(foo == "YES"){
-      digitalWrite(LED_BUILTIN, HIGH);
-    } else if(foo == "NO"){
-      digitalWrite(LED_BUILTIN, LOW);
+  for(int i = 0; i < num_pins; i++){
+    if(digitalRead(pins[i]) == LOW){
+      if(!key_down[i]){
+        Keyboard.write(alpha_1[i]);
+      }
+      key_down[i] = true;
+    } else {
+      key_down[i] = false;
     }
   }
-  delay(100);
+
+  delay(10);
 
 }
+
--- keyboard/wired/right.ino
+++ keyboard/wired/right.ino
@@ -1,23 +1,78 @@
 // RIGHT HALF
+#include <Keyboard.h>
 
 #define rx_pin 1
 #define tx_pin 0
+
+char layout_alpha_2_key = 'ÿ';
+char layout_symbol_key = 'þ';
+char layout_num_key = 'ý';
+char layout_system_key = 'ü';
+
+int pins[] = {
+  10, 11, 12, 13,
+  21, 20, 19, 18,
+  16, 17
+};
+const int num_pins = sizeof(pins)/sizeof(pins[0]);
+
+char key_down[num_pins];
+
+char alpha_1[] = {
+  'h', 'u', 'o', layout_system_key,
+  'n', 'e', 'a', 'c',
+  ' ', layout_alpha_2_key
+};
+
+char alpha_2[] = {
+  'k', '\'', 'z', '"',
+  'j', ',', 'x', 'y',
+  KEY_CAPS_LOCK, KEY_TAB
+};
+
 
 void setup() {
 
   Serial1.setRX(rx_pin);
   Serial1.setTX(tx_pin);
   Serial1.begin(115200);
+  Keyboard.begin();
+
+  for(int i = 0; i < num_pins; i++){
+    pinMode(pins[i], INPUT_PULLUP);
+    key_down[i] = false;
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
 
-  if(Serial1.availableForWrite()){
-    Serial1.write("YES\n");
-    delay(1000);
-    Serial1.write("NO\n");
-    delay(1000);
+  for(int i = 0; i < num_pins; i++){
+    if(digitalRead(pins[i]) == LOW){
+      
+      // ALPHA 2
+      if(digitalRead(17) == LOW){
+        if(!key_down[i] && alpha_1[i] != layout_alpha_2_key){
+          Keyboard.write(alpha_2[i]);
+        }
+        key_down[i] = true;
+      } 
+      // ALPHA 1
+      else {
+        if(!key_down[i]){
+          Keyboard.write(alpha_1[i]);
+        }
+        key_down[i] = true;
+      }
+
+    } else {
+      key_down[i] = false;
+    }
   }
 
+  delay(10);
+
 }
+
