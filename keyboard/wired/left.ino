3d76c6bcc75f0a72448e2aa88c9df2a8a56fd38d
Aditya Gautam <adityascottish27@gmail.com>
2024-07-28 17:41:53 +0530

    Updated

--- keyboard/wired/left.ino
+++ keyboard/wired/left.ino
@@ -1,62 +1,23 @@
-// LEFT HALF
-#include <Keyboard.h>
+#include <Wire.h>
 
-#define rx_pin 1
-#define tx_pin 0
+const int pin = 21;
+const int sda_pin = 0;
+const int scl_pin = 1;
+const int i2c_addr = 0x08;
 
-char layout_alpha_2_key = 'ÿ';
-char layout_symbol_key = 'þ';
-char layout_num_key = 'ý';
-char layout_system_key = 'ü';
-
-int pins[] = {
-  18, 19, 20, 21,
-  13, 12, 11, 10,
-          14, 15
-};
-const int num_pins = sizeof(pins)/sizeof(pins[0]);
-
-char key_down[num_pins];
-
-char alpha_1[] = {
-  layout_num_key, 'l', 'g', 'd',
-             'i', 's', 'r', 't',
-             layout_alpha_2_key, KEY_BACKSPACE
-};
-
-
-String foo;
-
-void setup() {
-
-  Serial1.setRX(rx_pin);
-  Serial1.setTX(tx_pin);
-  Serial1.begin(115200);
-  Keyboard.begin();
-
-  for(int i = 0; i < num_pins; i++){
-    pinMode(pins[i], INPUT_PULLUP);
-    key_down[i] = false;
-  }
-
-  pinMode(LED_BUILTIN, OUTPUT);
-
+void setup(){
+  Wire.setSCL(scl_pin);
+  Wire.setSDA(sda_pin);
+  Wire.begin();
+  pinMode(pin, INPUT_PULLUP);
 }
 
-void loop() {
+void loop(){
+  Wire.beginTransmission(i2c_addr);
 
-  for(int i = 0; i < num_pins; i++){
-    if(digitalRead(pins[i]) == LOW){
-      if(!key_down[i]){
-        Keyboard.write(alpha_1[i]);
-      }
-      key_down[i] = true;
-    } else {
-      key_down[i] = false;
-    }
+  if(digitalRead(pin) == LOW){
+    Wire.write('a');
   }
 
-  delay(10);
-
+  Wire.endTransmission();
 }
-
--- keyboard/wired/right.ino
+++ keyboard/wired/right.ino
@@ -1,78 +1,33 @@
-// RIGHT HALF
+#include <Wire.h>
 #include <Keyboard.h>
 
-#define rx_pin 1
-#define tx_pin 0
+const int sda_pin = 0;
+const int scl_pin = 1;
+const int i2c_addr = 0x08;
 
-char layout_alpha_2_key = 'ÿ';
-char layout_symbol_key = 'þ';
-char layout_num_key = 'ý';
-char layout_system_key = 'ü';
+char rec;
 
-int pins[] = {
-  10, 11, 12, 13,
-  21, 20, 19, 18,
-  16, 17
-};
-const int num_pins = sizeof(pins)/sizeof(pins[0]);
+bool key_down = false;
 
-char key_down[num_pins];
-
-char alpha_1[] = {
-  'h', 'u', 'o', layout_system_key,
-  'n', 'e', 'a', 'c',
-  ' ', layout_alpha_2_key
-};
-
-char alpha_2[] = {
-  'k', '\'', 'z', '"',
-  'j', ',', 'x', 'y',
-  KEY_CAPS_LOCK, KEY_TAB
-};
-
-
-void setup() {
-
-  Serial1.setRX(rx_pin);
-  Serial1.setTX(tx_pin);
-  Serial1.begin(115200);
-  Keyboard.begin();
-
-  for(int i = 0; i < num_pins; i++){
-    pinMode(pins[i], INPUT_PULLUP);
-    key_down[i] = false;
-  }
-
+void setup(){
+  Wire.setSCL(scl_pin);
+  Wire.setSDA(sda_pin);
+  Wire.begin(i2c_addr);
   pinMode(LED_BUILTIN, OUTPUT);
-
 }
 
-void loop() {
-
-  for(int i = 0; i < num_pins; i++){
-    if(digitalRead(pins[i]) == LOW){
-      
-      // ALPHA 2
-      if(digitalRead(17) == LOW){
-        if(!key_down[i] && alpha_1[i] != layout_alpha_2_key){
-          Keyboard.write(alpha_2[i]);
-        }
-        key_down[i] = true;
-      } 
-      // ALPHA 1
-      else {
-        if(!key_down[i]){
-          Keyboard.write(alpha_1[i]);
-        }
-        key_down[i] = true;
-      }
-
-    } else {
-      key_down[i] = false;
+void loop(){
+  Wire.requestFrom(i2c_addr, 1);
+  rec = Wire.read();
+  if(rec == 'a'){
+    digitalWrite(LED_BUILTIN, HIGH);
+    if(!key_down){
+      Keyboard.write('a');
     }
+    key_down = true;
+  } else {
+    key_down = false;
+    digitalWrite(LED_BUILTIN, LOW);
   }
-
-  delay(10);
-
+  
 }
-
