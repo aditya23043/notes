f1adad052363c292e7367bdab6db9c7245d1e7ea
Aditya Gautam <adityascottish27@gmail.com>
2024-09-09 12:32:10 +0530

    tuf15

--- coding/flutter/bha/lib/HomePage.dart
+++ coding/flutter/bha/lib/HomePage.dart
@@ -1,4 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+
+
+final List<String> imgList = [
+  './assets/images/1.jpg',
+  './assets/images/2.jpg',
+  './assets/images/3.jpg',
+];
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,19 +14,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[100],
       appBar: AppBar(
         title: const Center(
           child: Text(
             "Happy Birthday Bro!!",
             style: TextStyle(
-              fontFamily: "Fira Sans",
-              fontWeight: FontWeight.w700,
+              fontFamily: "Dancing Script",
+              fontWeight: FontWeight.w100,
               fontSize: 25,
             ),
           ),
         ),
         backgroundColor: Colors.deepPurple,
         foregroundColor: Colors.white,
+      ),
+      body: Builder(
+        builder: (context) {
+          final double height = MediaQuery.of(context).size.height;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: height,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              autoPlay: true,
+              autoPlayCurve: Curves.easeInOutQuint,
+              autoPlayInterval: const Duration(seconds: 6),
+            ),
+            items: imgList.map((item) => Center(
+              child: Image.asset(
+                item, fit: BoxFit.cover, height: height,
+              )
+            )).toList(),
+          );
+        }
       ),
     );
   }
