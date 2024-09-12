2786f588fbb11aaaeabb6055573891c73ec1cf38
Aditya Gautam <adityascottish27@gmail.com>
2024-09-13 00:27:33 +0530

    asus tuf a15

---
+++ sem3/ELD2024_LAB/lab3_hw/lab3_hw.ip_user_files/ip/ila_0/ila_0_stub.vhdl
@@ -0,0 +1,34 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+-- Date        : Thu Sep  5 22:51:29 2024
+-- Host        : archlinux running 64-bit Arch Linux
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/adi/repo/notes/sem3/ELD2024_LAB/lab3_hw/lab3_hw.srcs/sources_1/ip/ila_0/ila_0_stub.vhdl
+-- Design      : ila_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ila_0 is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe0 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe1 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe2 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe3 : in STD_LOGIC_VECTOR ( 6 downto 0 )
+  );
+
+end ila_0;
+
+architecture stub of ila_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[0:0],probe1[0:0],probe2[0:0],probe3[6:0]";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "ila,Vivado 2019.1";
+begin
+end;
